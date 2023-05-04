22e60745f347da9d3ec43f09dca7fff6c9cbd4e1
md al-amin <muhammadalamin809@gmail.com>
2023-05-04 08:07:30 +0800

    show password btn added

--- src/Login/Login/Login.jsx
+++ src/Login/Login/Login.jsx
@@ -9,9 +9,9 @@ const Login = () => {
     const navigate = useNavigate();
     const location = useLocation();
     const from = location.state?.from?.pathname || '/';
-    console.log(from)
     const [error, setError] = useState('');
     const [emailError, setEmailError] = useState('');
+    const [passwordType, setPasswordType] = useState(true);
     const { signInUser, createUserWithGoogle, createUserWithGithub } = useContext(AuthContext);
 
     const handleSignIn = (event) => {
@@ -66,6 +66,7 @@ const Login = () => {
                 alert(error.message)
             })
     }
+
     return (
         <div className=' form-width mx-auto my-5 border rounded px-4 py-5 shadow' >
             <h3 className='text-primary'>Login</h3>
@@ -80,23 +81,23 @@ const Login = () => {
 
                 <Form.Group className="mb-3" controlId="formBasicPassword">
                     <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" placeholder="Password" name='password' required />
+                    <Form.Control type= {passwordType ? 'password' : 'text'} placeholder="Password" name='password' required />
                 </Form.Group>
                 <Form.Text className="text-muted">
                     <p className='text-danger'> {error}  </p>
                 </Form.Text>
                 <Form.Text className="text-muted">
-                    <p><button className='btn btn-link'>Show password</button></p>
+                    <p className='btn btn-link' onClick={() => setPasswordType(!passwordType)} > <small>{passwordType ? 'Show password' : 'Hide password'} </small></p>
                 </Form.Text>
                 <Button variant="primary fw-semibold w-100" type="submit">
                     Login
                 </Button>
                 <p className='text-center mt-3'><small>New to Chef Savvy? <Link to='/register'>Create new Account</Link></small> </p>
                 <div className="mt-5">
-                    <button onClick={handleSignInWithGoogle} className="btn border border-2 w-100 "> <img className='' style={{ width: '20px' }} src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/2008px-Google_%22G%22_Logo.svg.png" alt="" /> Continue with Google</button>
+                    <p onClick={handleSignInWithGoogle} className="btn border border-2 w-100 "> <img className='' style={{ width: '20px' }} src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/2008px-Google_%22G%22_Logo.svg.png" alt="" /> Continue with Google</p>
                 </div>
-                <div className="mt-3">
-                    <button onClick={handleSignInWithGithub} className="btn border border-2 w-100 "> <img className='' style={{ width: '30px' }} src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png" alt="" /> Continue with Github</button>
+                <div>
+                    <p onClick={handleSignInWithGithub} className="btn border border-2 w-100 "> <img className='' style={{ width: '30px' }} src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png" alt="" /> Continue with Github</p>
                 </div>
             </Form>
         </div>
--- src/Login/Register/Register.jsx
+++ src/Login/Register/Register.jsx
@@ -6,6 +6,7 @@ import { toast } from "react-toastify";
 
 const Register = () => {
     const [error, setError] = useState('');
+    const [passwordType, setPasswordType] = useState(true);
     const { createUser, createUserWithGoogle, createUserWithGithub, profileUpdate } = useContext(AuthContext);
 
     const handleRegister = (event) => {
@@ -74,13 +75,13 @@ const Register = () => {
 
                 <Form.Group className="mb-3" controlId="formBasicPassword">
                     <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" placeholder="Password" name='password' required />
+                    <Form.Control type= {passwordType ? 'password' : 'text'} placeholder="Password" name='password' required />
                 </Form.Group>
                 <Form.Text className="text-muted">
                     <p className="text-danger"><small>{error}</small></p>
                 </Form.Text>
                 <Form.Text className="text-muted">
-                    <p><button className='btn btn-link'>Show password</button></p>
+                    <p className='btn btn-link' onClick={() => setPasswordType(!passwordType)} > <small>{passwordType ? 'Show password' : 'Hide password'} </small></p>
                 </Form.Text>
                 <Form.Group className="mb-3" controlId="formBasicPassword">
                     <Form.Label>Phot URL</Form.Label>
@@ -95,10 +96,10 @@ const Register = () => {
                 </Button>
                 <p className='text-center mt-3'>Already Have an Account? <Link to='/login'>Login</Link> </p>
                 <div className="mt-5">
-                    <button onClick={handleRegisterWithGoogle} className="btn border border-2 w-100 "> <img className='' style={{ width: '20px' }} src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/2008px-Google_%22G%22_Logo.svg.png" alt="" /> Continue with Google</button>
+                    <p onClick={handleRegisterWithGoogle} className="btn border border-2 w-100 "> <img className='' style={{ width: '20px' }} src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/2008px-Google_%22G%22_Logo.svg.png" alt="" /> Continue with Google</p>
                 </div>
-                <div className="mt-3">
-                    <button onClick={handleRegisterWithGithub} className="btn border border-2 w-100 "> <img className='' style={{ width: '30px' }} src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png" alt="" /> Continue with Github</button>
+                <div>
+                    <p onClick={handleRegisterWithGithub} className="btn border border-2 w-100 "> <img className='' style={{ width: '30px' }} src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png" alt="" /> Continue with Github</p>
                 </div>
             </Form>
         </div>
